2620db00fb92e2fada15cf1dd1b6352819b34e6a
Carol Kithinji <carolkithinji35@gmail.com>
2026-06-20 20:37:14 +0300

    Fix typo in model not found message in app.py

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ def models(model):
         if model.lower() in existing_models:
             return f"Flatiron {model} is in our fleet!"
         else:
-            return f"No model called {model} exists in our catalog"
+            return f"No models called {model} exists in our catalog"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5555)
